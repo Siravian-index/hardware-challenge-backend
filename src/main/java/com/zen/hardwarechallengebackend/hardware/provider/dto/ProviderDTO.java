36310172e5bd6935348836d1deb33c755f4eb8a2
David <depch47@gmail.com>
2022-06-08 14:45:32 -0500

    ADD--configs && provider entity and dto

---
+++ src/main/java/com/zen/hardwarechallengebackend/hardware/provider/dto/ProviderDTO.java
@@ -0,0 +1,10 @@
+package com.zen.hardwarechallengebackend.hardware.provider.dto;
+
+import lombok.Data;
+
+@Data
+public class ProviderDTO {
+    private String id;
+    private String name;
+    private String card;
+}
