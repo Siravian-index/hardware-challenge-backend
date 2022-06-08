352b1a1382f6eeda978e0b631a5ab887f7b528fc
David <depch47@gmail.com>
2022-06-08 14:30:52 -0500

    starter files

---
+++ src/main/java/com/zen/hardwarechallengebackend/HardwareChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zen.hardwarechallengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareChallengeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareChallengeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zen/hardwarechallengebackend/HardwareChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zen.hardwarechallengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareChallengeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
